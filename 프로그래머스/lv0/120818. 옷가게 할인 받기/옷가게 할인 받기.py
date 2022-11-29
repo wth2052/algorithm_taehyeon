ff1d3dd55191b691bfcb9602f8743928de63af6f
wth2052 <55482454+wth2052@users.noreply.github.com>
2022-11-29 17:53:30 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,12 +1,11 @@
 import math
 
-
 def solution(price):
     if 100000 <= price <= 299999:
         answer = price * 0.95
     elif 300000 <= price <= 499999:
         answer = price * 0.90
-    elif 499999 < price:
+    elif 499999 <= price:
         answer = price * 0.80
     elif 10 <= price <= 99999:
         answer = price
